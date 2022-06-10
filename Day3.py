9f8030d83dbe3a1b2564e6a60001b6a8fb9b8cb9
Abtin Ijadi Maghsoodi <aimaghsoodi@outlook.com>
2022-06-10 12:33:23 +1200

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -5,6 +5,21 @@ if height >= 120:
   print("You can ride the rollercoster!")
 else:
   print("Sorry, you have to grow taller before you can ride")
+  
+# or the following:
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120:
+  print("You can ride the rollercoster!")
+  age = int(input("What is your age?"))
+  if age <= 12:
+    print("Please pay $5")
+  elif age <= 18:
+    print("Please pay $7")
+  else:
+    print("Please pay $12")
+else:
+  print("Sorry, you have to grow taller before you can ride")
 
 #2: Odd or Even:
 number = int(input("Which number do you want to check?"))
