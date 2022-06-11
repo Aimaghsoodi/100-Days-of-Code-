c8570526a10cbbc3f1c72699ca967fac69e12213
Abtin Ijadi Maghsoodi <aimaghsoodi@outlook.com>
2022-06-12 00:26:04 +1200

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -42,13 +42,20 @@ names = names_string.split(", ")
 person_who_will_pay = random.choice(names)
 print(person_who_will_pay + " is going to buy the meal today!")
 
-#4: 
+#4: Treasure Map:
+# You are going to write a program which will mark a spot with an X.
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+horizontal = int(position[0])
+vertical = int(position[1])
+map[vertical - 1][horizontal - 1] = "X"
+print(f"{row1}\n{row2}\n{row3}")
 
-
-
-
-
-# Rock/Paper/Scissors Program: 
+#5: Rock/Paper/Scissors Program: 
 
 import random
 
