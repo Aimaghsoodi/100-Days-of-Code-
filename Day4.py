f33e305c2fa8820bceb43b3ad1325f810f1485f3
Abtin Ijadi Maghsoodi <aimaghsoodi@outlook.com>
2022-06-11 19:55:38 +1200

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -10,7 +10,39 @@ random_int = rd.randint(1,100)
 print(random_int)
 
 #2: Heads or Tails:
+import random 
+random_side = random.randint(0,1)
+if random_side ==1:
+  print("Heads")
+else:
+  print("Tails")
 
+# On data structures: https://docs.python.org/3/tutorial/datastructures.html
+  
+#3: Banker Roullete:
+import random
+# Split string method
+# You are going to write a program which will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+names_string = input("Give me everybody's names, separated by a comma. ")
+# split is a devider function 
+names = names_string.split(", ")
+#Get the total number of items in list.
+num_items = len(names)
+#Generate random numbers between 0 and the last index. 
+random_choice = random.randint(0, num_items - 1)
+#Pick out random person from list of names using the random number.
+person_who_will_pay = names[random_choice]
+print(person_who_will_pay + " is going to buy the meal today!")
+
+# OR: 
+import random
+names_string = input("Give me everybody's names, separated by a comma. ")
+# split is a devider function 
+names = names_string.split(", ")
+person_who_will_pay = random.choice(names)
+print(person_who_will_pay + " is going to buy the meal today!")
+
+#4: 
 
 
 
