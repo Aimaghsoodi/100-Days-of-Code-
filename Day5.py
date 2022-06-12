098062eb5390281957e1ce80f8241802ce143831
Abtin Ijadi Maghsoodi <aimaghsoodi@outlook.com>
2022-06-12 17:21:30 +1200

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -68,9 +68,19 @@ for number in range(1,101):
 print(total2)
 
 #5: FizBuzz Job Interview:
-
-
-
+# https://en.wikipedia.org/wiki/Fizz_buzz
+# You are going to write a program that automatically prints the solution to the FizzBuzz game.
+# Your program should print each number from 1 to 100 in turn.
+# When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
 
 #6: Password Generator Project:
 import random
