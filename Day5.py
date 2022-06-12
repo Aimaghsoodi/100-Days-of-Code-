ef7ddc5dab91a98cd8a98b588f139e6e9e7e56af
Abtin Ijadi Maghsoodi <aimaghsoodi@outlook.com>
2022-06-12 16:45:29 +1200

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -2,8 +2,33 @@
 fruits = ["Apple","Peach","Pear"]
 for fruit in fruits:
   print(fruit)
+  print(fruit + "Pie")
   
 #2: Average Height:
+# You are going to write a program that calculates the average student height from a List of heights.
+# Demo numbers: 156 178 165 171 187
+student_heights = input("Input a list of student heights ").split()
+
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+print(student_heights)
+
+total_height = 0
+for height in student_heights:
+  total_height += height
+print(f"total height = {total_height}")
+
+number_of_students = 0
+for student in student_heights:
+  number_of_students += 1
+print(f"number of students = {number_of_students}")
+  
+average_height = round(total_height / number_of_students)
+print(average_height)
+
+#3: High Score: 
+
+
 
 
 
